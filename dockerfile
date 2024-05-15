3bfd38b94546f7442a75431599e4b5801b114135
fathankep0 <sholihin.013@gmail.com>
2024-05-15 19:37:27 +0700

    first commit

---
+++ dockerfile
@@ -0,0 +1,22 @@
+FROM golang:alpine AS builder
+
+WORKDIR /app
+
+COPY main.go .
+
+RUN go mod init test
+
+RUN go build -o test .
+
+FROM alpine
+
+RUN apk --no-cache add ca-certificates
+
+WORKDIR /app
+
+COPY --from=builder /app/test .
+
+# Menambahkan environment variable
+ENV Environment_Name=Production
+
+CMD ["./test"]
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Nama Environment bisa diambil dari environment variable
+	environmentName := os.Getenv("Environment_Name")
+
+	fmt.Fprintf(w, "<h1>Nama Aplikasi: Test Devsecops Batch-11</h1>")
+	fmt.Fprintf(w, "<p>Nama Environment: %s</p>", environmentName)
+	fmt.Fprintf(w, "<p>Version: 1.0</p>")
+	fmt.Fprintf(w, "<p>Status: OK</p>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
+}
